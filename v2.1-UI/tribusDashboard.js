75795da42861b6177b47b988914c57c0eafd270f
charmful0x <77340894+charmful0x@users.noreply.github.com>
2021-06-12 22:14:51 +0300

    feat: PSC Metrics

--- v2.1-UI/tribusDashboard.js
+++ v2.1-UI/tribusDashboard.js
@@ -7,12 +7,19 @@ const readContract = smartweave.readContract
 const tribusID = window.location.hash.substring(1);
 
 let TRIBUSES_STATE;
-
+//tribus metrics
 const tribusName = document.getElementById("tribusName");
 const tribusBio = document.getElementById("bio");
 const tribusMembership = document.getElementById("tribusMembership");
 const tribusPostVisibility = document.getElementById("tribusPostVisibility");
 const tribusLastUpdate = document.getElementById("tribusLastUpdate");
+// PSC metrics
+const pscLogo = document.getElementById("logo");
+const pscVotes = document.getElementById("votes");
+const pscTicker = document.getElementById("ticker");
+const pscCxyz = document.getElementById("cxyz");
+const pscVerto = document.getElementById("trade-on-verto");
+const pscHolders = document.getElementById("holders");
 
 
 async function arweaveNetwork() {
@@ -31,9 +38,11 @@ async function get_tribus(tribusID) {
 		swal({title: "Tribus Not Found", text: "invalid Tribus ID", icon: "error"})
 		return
 	}
-
+	const psc = await get_psc_data(tribusID);
+	
 	await parse_tribus_name(TRIBUSES_STATE, tribusID)
 	await parse_tribus_idendity(TRIBUSES_STATE, tribusID)
+	await parse_psc_data(psc)
 
 }
 
@@ -54,6 +63,40 @@ async function parse_tribus_idendity(state, tribusID) {
 	tribusPostVisibility.innerHTML = `Post Visibility: ${state["tribuses"][tribusID]["visibility"]}`
 	tribusLastUpdate.innerHTML = `Last Update ID: <a href="${viewblock}/tx/${lastUpdateTX}">${lastUpdateTX}`
 }
+
+async function parse_psc_data(pscObject) {
+
+	const {logo, ticker, votesCount} = pscObject;
+	// links
+	const gateway = "https://arweave.net"
+	const verto = "https://verto.exchange/token?id="
+	const communityXYZ = "https://community.xyz/#"
+	const viewblockHolders = "https://viewblock.io/arweave/address/"
+
+	pscLogo.innerHTML = `<img style="max-height: 100px; min-height: 100px" src="${gateway}/${logo}">`
+	pscVotes.innerHTML = `Votes Count: ${votesCount}`
+	pscTicker.innerHTML = `PSC Ticker: $${ticker}`
+	pscVerto.innerHTML = `<button onclick="window.location.href='${verto}${tribusID}'">Trade On Verto</button>`
+	pscCxyz.innerHTML = `<button onclick="window.location.href='${communityXYZ}${tribusID}'">CommunityXYZ</button>`
+	pscHolders.innerHTML = `<button onclick="window.location.href='${viewblockHolders}${tribusID}?tab=holders'">Holders List</button>`
+
+
+}
+
+async function get_psc_data(tribusID) {
+	const pscState = await readContract(arweave, tribusID);
+
+	const logo = pscState["settings"][5][1]
+	const ticker = pscState["ticker"]
+	const votesCount = pscState["votes"].length
+
+	return {
+		logo,
+		ticker,
+		votesCount
+	}
+}
+
 
 get_tribus(tribusID)
 setInterval(arweaveNetwork, 3000)
